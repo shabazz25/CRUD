670ba04d13c83be5ce10b1067069d64136cd3070
André Pereira Pinheiro <42389068+shabazz25@users.noreply.github.com>
2022-03-01 21:00:15 -0300

    main

--- main.js
+++ main.js
@@ -6,6 +6,51 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+
+//JSON Cliente
+const tempClient = {
+    nome: "Andrédddw",
+    email: "andre@gmail.com",
+    celular: "11951728549",
+    cidade: "São Roque"
+}
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem('dbClient')) ?? []
+//JSON.stringfy transforma o objeto em string
+const setLocalStorage = (dbClient) => localStorage.setItem("dbClient", JSON.stringify(dbClient))
+
+
+const deleteClient = (index) =>{
+    const dbClient = readClient()
+        dbClient.splice(index, 1)
+        setLocalStorage(dbClient)
+    
+
+   
+}
+
+const updateClient = (index, client) => {
+const dbClient= readClient()
+dbClient[index]= client
+setLocalStorage(dbClient)
+
+}
+
+//CRUD - Ler CLIENTE
+const readClient = () => getLocalStorage()
+
+//CRUD - Função criar Cliente
+const createClient = (client) => {
+    const dbClient = getLocalStorage()
+    //console.log(dbClient)
+    dbClient.push(client)
+    setLocalStorage(dbClient)
+
+
+
+}
+
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
